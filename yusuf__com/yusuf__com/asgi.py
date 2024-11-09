62bf722e149d8fadb3626192335e8f66c29a7d6d
1yusufcelik2 <clkysf19@gmail.com>
2024-11-09 23:00:54 +0300

    ilk commit

---
+++ yusuf__com/yusuf__com/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for yusuf__com project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yusuf__com.settings')
+
+application = get_asgi_application()
